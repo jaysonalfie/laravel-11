7167c86ac8886cf6851941bfabf573982c45fe2b
jaysonalfie <jaysonalfie@gmail.com>
2024-10-21 14:51:42 +0300

    learnt about model factories for scafolding data and for tests

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 
 //declaring array so as not to duplicate 
 //create a class that retrieves all
 class Job extends Model{ 
+
+  use HasFactory;
   protected $table= 'job_listings';
 
   //elements that can be mass assigned
